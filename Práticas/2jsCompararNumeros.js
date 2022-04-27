f287735b07c6d89f80a7b9c06d88eaeef88ab870
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-26 23:03:42 -0300

    create file

---
+++ Práticas/2jsCompararNumeros.js
@@ -0,0 +1,19 @@
+var compara = 0;
+
+
+function comparaNum(num1,num2){
+    if(num1 == num2){
+        compara++;
+    }
+    return num1,num2
+}
+
+function comparaSoma(num1,num2){
+    var soma = num1 + num2 
+    if(soma > 10 ){
+
+    }
+    (soma < 20) ? {}
+}
+
+console.log("Os números " + comparaNum(imput any,any))

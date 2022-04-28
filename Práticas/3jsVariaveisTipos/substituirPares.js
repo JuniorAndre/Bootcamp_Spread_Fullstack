e58ff8b737a2b380b0481bd8fe8b54a826a3dcf3
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-28 20:24:51 -0300

    create file

---
+++ Práticas/3jsVariaveisTipos/substituirPares.js
@@ -0,0 +1,10 @@
+let myArray = [1, 3, 4, 6, 80, 33, 23, 90];
+
+function trocarPares(array){
+    for(let i=0; i<array.length; i++){
+        if(array[i] % 2 = 0){
+            array[i] = 0
+        }
+    }
+}
+console.log(trocarPares(myArray) + myArray)

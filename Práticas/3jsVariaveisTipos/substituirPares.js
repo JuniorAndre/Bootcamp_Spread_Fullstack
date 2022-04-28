422f211370e4201c3fc1112b926d08cd186fddd9
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-28 20:36:15 -0300

    update

--- Práticas/3jsVariaveisTipos/substituirPares.js
+++ Práticas/3jsVariaveisTipos/substituirPares.js
@@ -2,9 +2,15 @@ let myArray = [1, 3, 4, 6, 80, 33, 23, 90];
 
 function trocarPares(array){
     for(let i=0; i<array.length; i++){
-        if(array[i] % 2 = 0){
+        if(array[i] % 2 == 0){
             array[i] = 0
         }
     }
+    if(array.length == 0){
+        return -1;
+    }else{
+        console.log(array)
+    }
 }
-console.log(trocarPares(myArray) + myArray)
+
+trocarPares(myArray);

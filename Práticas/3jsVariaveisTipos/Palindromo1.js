52639e7e5787bfb7eaf6125d12c93510665721c1
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-28 18:37:15 -0300

    create palindromo

---
+++ Práticas/3jsVariaveisTipos/Palindromo1.js
@@ -0,0 +1,25 @@
+var array = [];
+
+function palindromo(string){
+    let tamanhoString = string.length;
+    let primeiraLetra = string[0];
+    let ultimaLetra = string[string.length];
+    for(let i=0; i < string.length; i++){
+        for(let j=string.length; j > 0; j--){
+            let primeiraLetra = string[i];
+            let ultimaLetra = string[j];
+            if(primeiraLetra == ultimaLetra){
+                array.push(1);
+            }else{
+                array.push(0);
+            }
+        }
+    }
+    if(array.every(item => item == 1)){
+        console.log(string + " é um palíndromo");
+    }else{
+        console.log(string + " não é um palíndromo");
+    }
+}
+
+console.log(palindromo("ovo"));

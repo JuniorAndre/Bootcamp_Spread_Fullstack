c8b789f9466ecb13dacc00c729090c740d95974d
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-28 19:13:39 -0300

    update

--- Práticas/3jsVariaveisTipos/Palindromo1.js
+++ Práticas/3jsVariaveisTipos/Palindromo1.js
@@ -1,17 +1,19 @@
 var array = [];
 
 function palindromo(string){
+    let j = 0;
     let tamanhoString = string.length;
     let primeiraLetra = string[0];
     let ultimaLetra = string[string.length];
-    for(let i=0; i < string.length; i++){
-            let primeiraLetra = string[i];
-            let ultimaLetra = string[j];
+    for(let i=string.length - 1; i >= 0; i--){
+            let primeiraLetra = string[j];
+            let ultimaLetra = string[i];
             if(primeiraLetra == ultimaLetra){
                 array.push(1);
             }else{
                 array.push(0);
             }
+            j += 1;
     }
     if(array.every(item => item == 1)){
         console.log(string + " é um palíndromo");
@@ -20,4 +22,4 @@ function palindromo(string){
     }
 }
 
-console.log(palindromo("ovo"));
+palindromo("subi no onibus");

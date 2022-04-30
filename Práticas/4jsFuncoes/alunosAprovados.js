d7fe6d1db77975afd0e541c8e1ba6133dd57d762
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-30 11:32:12 -0300

    update

--- Práticas/4jsFuncoes/alunosAprovados.js
+++ Práticas/4jsFuncoes/alunosAprovados.js
@@ -1,36 +1,36 @@
 const alunos = [
     {
-        Nome: Clara,
+        Nome: 'Clara',
         Nota: 65,
-        Turma: 1B,
+        Turma: '1B',
     },
     {
-        Nome: Pedro,
+        Nome: 'Pedro',
         Nota: 50,
-        Turma: 1B,
+        Turma: '1B',
     },
     {
-        Nome: Daniel,
+        Nome: 'Daniel',
         Nota: 89,
-        Turma: 2C,
+        Turma: '2C',
     },
     {
-        Nome: Guto,
+        Nome: 'Guto',
         Nota: 76,
-        Turma: 2C,
+        Turma: '2C',
     }
 ]
 
 function alunosAprovados(array, media){
     let aprovados = [];
     for(let i=0; i < array.length; i++){
-        if(array[i] == media){
-            console.log(array[i] + ' foi aprovado!')
+        if(array[i].Nota > media){
+            console.log(array[i].Nome + ' foi aprovado!')
             array.push(aprovados);
             console.log(aprovados);
         }
     }
 
 }
-num  = 7;
+num = 70;
 alunosAprovados(alunos, num);
